b4a0772a7a908fd27a1787340b683e6e00b6b04c
ollie robins <ollierobins93@gmail.com>
2021-01-02 17:13:28 +0000

    Added Redux and store and user reducer. Also added redirect for when people use the login feature

--- src/App.js
+++ src/App.js
@@ -1,44 +1,70 @@
-import React, { useState, useEffect } from "react";
+import React from 'react';
 import './app.css';
 import Homepage from "./pages/Homepage";
 import Shop from './pages/Shop';
-import { Route , Switch } from 'react-router-dom';
+import { Route , Switch, Redirect } from 'react-router-dom';
 import Header from './components/Header';
 import Login from './pages/Login';
 import { auth, createUserProfileDocument } from './Firebase/firebase.utils';
+import { connect } from 'react-redux';
+import  setCurrentUser  from './Redux/User/user.actions';
 
-function App() {
+class App extends React.Component {
+  unsubscribeFromAuth = null;
 
-  const [currentUser, setCurrentUser] = useState({id: ''})
+  componentDidMount() {
+    const { setCurrentUser } = this.props;
 
-  useEffect(() => {
-    auth.onAuthStateChanged( async userAuth => {
+    this.unsubscribeFromAuth = auth.onAuthStateChanged(async userAuth => {
       if (userAuth) {
         const userRef = await createUserProfileDocument(userAuth);
 
         userRef.onSnapshot(snapShot => {
           setCurrentUser({
             id: snapShot.id,
-              ...snapShot.data()
-            })
-        });        
-      } else {
-          setCurrentUser(userAuth)
-        }
-    })
-  }, []);
+            ...snapShot.data()
+          });
+        });
+      }
 
-  return (
-    <div className="App">
-      <Header currentUser={currentUser}/>
-      <Switch>
-        <Route exact path='/' component={Homepage}/>
-        <Route path='/shop' component={Shop}/>
-        <Route path='/Login' component={Login}/>
-        <Route path='/Contact' />
-      </Switch>
-    </div>
-  );
+      setCurrentUser(userAuth);
+    });
+  }
+
+  componentWillUnmount() {
+    this.unsubscribeFromAuth();
+  }
+
+  render() {
+    return (
+      <div>
+        <Header />
+        <Switch>
+          <Route exact path='/' component={Homepage} />
+          <Route path='/shop' component={Shop} />
+          <Route path='/login'
+            render = {() => this.props.currentUser ? (
+              <Redirect to='/'/>
+            ) : (
+              <Login/>
+            ) 
+            }
+          />
+        </Switch>
+      </div>
+    );
+  }
 }
 
-export default App;
+const mapStateToProps = ({user}) => ({
+  currentUser : user.currentUser
+});
+
+const mapDispatchToProps = dispatch => ({
+  setCurrentUser: user => dispatch(setCurrentUser(user))
+});
+
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(App);
